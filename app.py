ffe2a387338fb6eaf676bffa291f5099a21057b3
Gijs <gijstenberg@gmail.com>
2026-05-14 12:57:10 +0200

    Add save to new

--- app.py
+++ app.py
@@ -1,13 +1,15 @@
 from flask import (
     Flask,
     render_template,
+    request,
+    redirect,
+    url_for,
 )
 
 import os
-
 from dotenv import load_dotenv
 
-from models import db, Palette
+from models import db, Palette, Color
 
 load_dotenv(override=True)
 
@@ -36,6 +38,30 @@ def index():
 @app.get("/new")
 def new():
     return render_template("new.jinja")
+
+
+@app.post("/new")
+def submit_new():
+    colors = request.form.getlist("colors")
+    name = request.form.get("name")
+
+    palette = Palette(name=name)
+
+    db.session.add(palette)
+    db.session.flush()  # gets palette.id before commit
+
+    for index, hex_value in enumerate(colors):
+        color = Color(
+            palette_id=palette.id,
+            hex_value=hex_value,
+            position=index,
+        )
+
+        db.session.add(color)
+
+    db.session.commit()
+
+    return redirect(url_for("index"))
 
 
 if __name__ == "__main__":
